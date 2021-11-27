a86cd3dfec75ed3d07fbda788b7a3d4bfaf4a877
Tamika Crouch <thomthom1@live.co.uk>
2021-11-27 18:52:30 +0000

    Added .gitignore

--- buildAsPlugin.ps1
+++
@@ -1,9 +0,0 @@
-# Rojo Build Script - metatablecat
-# Licensed under Apache 2.0
-
-$name = "CommandBarBridgeInjector" # Model Name
-$extension = "rbxmx" # Model Extension, can be rbxm or rbxmx, do not include the .
-$path = "$env:localappdata/Roblox/Plugins" # Path of where to build the model to
-
-$path_final = "$path/$name.$extension"
-rojo build --output $path_final
