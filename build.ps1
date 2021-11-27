1017413e7d75da9edaf0171ccb54ec428c1681a0
Tamika Crouch <thomthom1@live.co.uk>
2021-11-27 18:53:47 +0000

    Update .gitignore

--- build.ps1
+++
@@ -1,9 +0,0 @@
-# Rojo Build Script - metatablecat
-# Licensed under Apache 2.0
-
-$name = "Plugin" # Model Name
-$extension = "rbxm" # Model Extension, can be rbxm or rbxmx, do not include the .
-$path = "./" # Path of where to build the model to
-
-$path_final = "$path/$name.$extension"
-rojo build --output $path_final
