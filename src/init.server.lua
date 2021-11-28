0d0d4410342d9d1c8fbf5903d5e263e8173baac4
Tamika Crouch <thomthom1@live.co.uk>
2021-11-28 19:55:55 +0000

    Add removal and reload warnings

--- src/init.server.lua
+++ src/init.server.lua
@@ -3,6 +3,7 @@
 
 local CoreGui = game:GetService("CoreGui")
 local activeModule
+local silenceManualDeletionWarning = false
 
 local function RequestScriptAccessEarly()
 	local didPerformAction = pcall(function()
@@ -24,5 +25,27 @@ if findModuleAlready then
 	return
 end
 
-activeModule = script.CommandBarBridge:Clone()
-activeModule.Parent = CoreGui
+local function deployModule()
+	activeModule = script.CommandBarBridge:Clone()
+	activeModule.Parent = CoreGui
+
+	activeModule.AncestryChanged:Connect(function(_, parent)
+		if parent == nil and not silenceManualDeletionWarning then
+			warn("The module was deleted. Please do not do this manually as it can create unhandlable issues.")
+			deployModule()
+		end
+	end)
+end
+
+plugin.Unloading:Connect(function()
+	silenceManualDeletionWarning = true
+
+	if activeModule then
+		activeModule:Destroy()
+		activeModule = nil
+	end
+
+	warn("The plugin was unloaded. Plugins that rely on this will not update until they are also reloaded.")
+end)
+
+deployModule()
